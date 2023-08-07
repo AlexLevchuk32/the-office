86c2831421de9673ebdd0c6f44eb27f67bdf74ea
Alex Levchuk <alexlevchuk@mail.ru>
2023-08-07 20:42:53 +0300

    used dynamic styles

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,12 +2,12 @@ import './app-filter.css';
 
 const AppFilter = (props) => {
 	const buttonsData = [
-		{ name: 'all', label: 'Все сотрудники' },
-		{ name: 'rise', label: 'На повышение' },
-		{ name: 'moreThen1000', label: 'З/п больше 1000$' },
+		{ name: 'all', label: 'Все сотрудники', colored: false },
+		{ name: 'rise', label: 'На повышение', colored: false },
+		{ name: 'moreThen1000', label: 'З/п больше 1000$', colored: true },
 	];
 
-	const buttons = buttonsData.map(({ name, label }) => {
+	const buttons = buttonsData.map(({ name, label, colored }) => {
 		const active = props.filter === name;
 		const clazz = active ? 'btn-light' : 'btn-outline-light';
 
@@ -17,6 +17,7 @@ const AppFilter = (props) => {
 				type="button"
 				key={name}
 				onClick={() => props.onFilterSelect(name)}
+				style={colored ? { color: 'red' } : null}
 			>
 				{label}
 			</button>
